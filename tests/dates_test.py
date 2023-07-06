2e3811b3d961822b999520047f7226689c453f5d
Iagocanalejas <iagocanalejas@gmail.com>
2023-07-06 21:34:58 +0200

    BATMAN

---
+++ tests/dates_test.py
@@ -0,0 +1,10 @@
+import unittest
+
+from pyutils.strings.datetime import find_date
+
+
+class TestDates(unittest.TestCase):
+    def test_find_dates(self) -> None:
+        dates = ["15/11/2005", "15/11/05", "25 July 2023", "29 ENERO 2023"]
+        for d in dates:
+            self.assertIsNotNone(find_date(d))
