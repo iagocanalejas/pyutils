09a879d0aecade68e4189e1aac237ce5150009f6
Iagocanalejas <iagocanalejas@gmail.com>
2023-07-06 21:34:58 +0200

    BATMAN

---
+++ pyutils/shortcuts/__init__.py
@@ -0,0 +1,17 @@
+from .checks import (
+    none as none,
+    all_none as all_none,
+    all_or_none as all_or_none,
+    all_not_none as all_not_none,
+    only_one_not_none as only_one_not_none,
+)
+from .dates import (
+    week_to_date as week_to_date,
+    weeks_between as weeks_between,
+    week_range_from_date as week_range_from_date,
+    date_range as date_range,
+)
+from .utils import (
+    generate_unique_code as generate_unique_code,
+    random_user_agent as random_user_agent,
+)
