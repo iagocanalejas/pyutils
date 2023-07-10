b2512b3d4a872e553826e8ea4db4da2a4b45657c
Iagocanalejas <iagocanalejas@gmail.com>
2023-07-10 13:45:45 +0200

    feat: email and dni validators

--- validators/__init__.py
+++ validators/__init__.py
@@ -1,8 +1,12 @@
 from .bank import BICValidator, IBANValidator
-from .dni import DNIValidator
+from .dni import DNIValidator, is_valid_dni
+from .email import is_valid_email
 
 
 __all__ = [
-    BICValidator, IBANValidator,
+    BICValidator,
+    IBANValidator,
     DNIValidator,
+    is_valid_dni,
+    is_valid_email,
 ]
