35f9c7fdd6bbca14a75c59ed1751b91d1a714c81
Iagocanalejas <iagocanalejas@gmail.com>
2023-07-20 16:32:37 +0200

    fix: missing import

--- pyutils/strings/__init__.py
+++ pyutils/strings/__init__.py
@@ -3,6 +3,7 @@ from .cleanup import (
     remove_symbols,  # pyright: ignore
     remove_parenthesis,  # pyright: ignore
     remove_conjunctions,  # pyright: ignore
+    remove_roman,  # pyright: ignore
 )
 from .dates import find_date  # pyright: ignore
 from .roman import int_to_roman, roman_to_int, find_roman  # pyright: ignore
