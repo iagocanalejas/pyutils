0d6c078edcab9404a429d9a480252c57542a24d2
Iagocanalejas <iagocanalejas@gmail.com>
2023-08-10 11:36:39 +0200

    feat: more date supported formats

---
+++ tests/dates_test.py
@@ -0,0 +1,9 @@
+import unittest
+from pyutils.strings.dates import find_date
+
+
+class TestDates(unittest.TestCase):
+    def test_find_dates(self):
+        dates = ["15/11/2005", "15/11/05", "25 July 2023"]
+        for d in dates:
+            self.assertIsNotNone(find_date(d))
