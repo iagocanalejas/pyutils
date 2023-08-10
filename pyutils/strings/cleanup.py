20e96c4ff62d9c8c907e7b2fef6577ad0fd16209
Iagocanalejas <iagocanalejas@gmail.com>
2023-08-10 17:33:20 +0200

    add <> to remove_symbols cleanup

--- pyutils/strings/cleanup.py
+++ pyutils/strings/cleanup.py
@@ -11,7 +11,7 @@ def remove_parenthesis(word: str) -> str:
 
 
 def remove_symbols(word: str, ignore_quotes: bool = False) -> str:
-    reg = r'[^\S\-"\']' if ignore_quotes else r"[^\S\-]"
+    reg = r'[^\S\-"\'<>]' if ignore_quotes else r"[^\S\-<>]"
     return whitespaces_clean(re.sub(reg, " ", word))
 
 
